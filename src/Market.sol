9efdd27a11943077c218a680f9e49c09c2a5974e
lazaralex98 <alex2012ro@gmail.com>
2025-05-19 20:20:28 +0300

    add owner

--- src/Market.sol
+++ src/Market.sol
@@ -3,8 +3,10 @@ pragma solidity ^0.8.13;
 
 contract Market {
     string public name;
+    address public owner;
 
     constructor(string memory _name) {
         name = _name;
+        owner = msg.sender;
     }
 }
