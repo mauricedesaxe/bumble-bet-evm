8eee3a1bd5e59775d8a4ee75547547b1227c5960
lazaralex98 <alex2012ro@gmail.com>
2025-05-19 20:55:38 +0300

    add basic order creation

--- src/Market.sol
+++ src/Market.sol
@@ -1,9 +1,43 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+enum BuySell {
+    BUY,
+    SELL
+}
+
+enum YesNo {
+    YES,
+    NO
+}
+
+enum LimitMarket {
+    LIMIT,
+    MARKET
+}
+
+enum OrderStatus {
+    PENDING,
+    FILLED,
+    CANCELLED
+}
+
+struct Order {
+    address user;
+    uint256 amount;
+    uint256 price;
+    BuySell side;
+    YesNo yesNo;
+    LimitMarket limitMarket;
+    OrderStatus status;
+}
+
 contract Market {
     string public name;
     address public owner;
+
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
 
     constructor(string memory _name) {
         name = _name;
@@ -15,5 +49,32 @@ contract Market {
             revert("Only the owner can set the name");
         }
         name = _name;
+    }
+
+    function createOrder(BuySell _side, YesNo _yesNo, LimitMarket _limitMarket, uint256 _amount, uint256 _price)
+        public
+    {
+        if (_amount == 0) {
+            revert("Amount must be greater than zero");
+        }
+        if (_price == 0) {
+            revert("Price must be greater than zero");
+        }
+
+        if (_side == BuySell.SELL) {
+            // TODO check if user does have the balance to sell
+            revert("Sell is not allowed if you don't own shares");
+        }
+
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = Order({
+            user: msg.sender,
+            amount: _amount,
+            price: _price,
+            side: _side,
+            yesNo: _yesNo,
+            limitMarket: _limitMarket,
+            status: OrderStatus.PENDING
+        });
     }
 }
--- test/Market.sol
+++ test/Market.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Market} from "../src/Market.sol";
+import {Market, BuySell, LimitMarket, YesNo, OrderStatus, Order} from "../src/Market.sol";
 
 contract MarketTest is Test {
     Market public market;
@@ -24,5 +24,107 @@ contract MarketTest is Test {
         vm.prank(address(1));
         vm.expectRevert("Only the owner can set the name");
         market.setName("New Market");
+    }
+
+    // BUY-YES-LIMIT
+    function test_Market_createOrder_BuyYesLimit() public {
+        market.createOrder(BuySell.BUY, YesNo.YES, LimitMarket.LIMIT, 100, 100);
+        assertEq(market.orderCount(address(this)), 1);
+
+        (
+            address user,
+            uint256 amount,
+            uint256 price,
+            BuySell side,
+            YesNo yesNo,
+            LimitMarket limitMarket,
+            OrderStatus status
+        ) = market.orders(address(this), 1);
+
+        assertEq(user, address(this));
+        assertEq(uint256(side), uint256(BuySell.BUY));
+        assertEq(uint256(yesNo), uint256(YesNo.YES));
+        assertEq(uint256(limitMarket), uint256(LimitMarket.LIMIT));
+        assertEq(amount, 100);
+        assertEq(price, 100);
+        assertEq(uint256(status), uint256(OrderStatus.PENDING));
+    }
+
+    // BUY-YES-MARKET
+    function test_Market_createOrder_BuyYesMarket() public {
+        market.createOrder(BuySell.BUY, YesNo.YES, LimitMarket.MARKET, 100, 100);
+        assertEq(market.orderCount(address(this)), 1);
+
+        (
+            address user,
+            uint256 amount,
+            uint256 price,
+            BuySell side,
+            YesNo yesNo,
+            LimitMarket limitMarket,
+            OrderStatus status
+        ) = market.orders(address(this), 1);
+
+        assertEq(user, address(this));
+        assertEq(uint256(side), uint256(BuySell.BUY));
+        assertEq(uint256(yesNo), uint256(YesNo.YES));
+        assertEq(uint256(limitMarket), uint256(LimitMarket.MARKET));
+        assertEq(amount, 100);
+        assertEq(price, 100);
+        assertEq(uint256(status), uint256(OrderStatus.PENDING));
+    }
+
+    // BUY-NO-LIMIT
+    function test_Market_createOrder_BuyNoLimit() public {
+        market.createOrder(BuySell.BUY, YesNo.NO, LimitMarket.LIMIT, 100, 100);
+        assertEq(market.orderCount(address(this)), 1);
+
+        (
+            address user,
+            uint256 amount,
+            uint256 price,
+            BuySell side,
+            YesNo yesNo,
+            LimitMarket limitMarket,
+            OrderStatus status
+        ) = market.orders(address(this), 1);
+
+        assertEq(user, address(this));
+        assertEq(uint256(side), uint256(BuySell.BUY));
+        assertEq(uint256(yesNo), uint256(YesNo.NO));
+        assertEq(uint256(limitMarket), uint256(LimitMarket.LIMIT));
+        assertEq(amount, 100);
+        assertEq(price, 100);
+        assertEq(uint256(status), uint256(OrderStatus.PENDING));
+    }
+
+    // BUY-NO-MARKET
+    function test_Market_createOrder_BuyNoMarket() public {
+        market.createOrder(BuySell.BUY, YesNo.NO, LimitMarket.MARKET, 100, 100);
+        assertEq(market.orderCount(address(this)), 1);
+    }
+
+    // SELL-YES-LIMIT (should revert)
+    function test_Market_createOrder_SellYesLimit() public {
+        vm.expectRevert("Sell is not allowed if you don't own shares");
+        market.createOrder(BuySell.SELL, YesNo.YES, LimitMarket.LIMIT, 100, 100);
+    }
+
+    // SELL-YES-MARKET (should revert)
+    function test_Market_createOrder_SellYesMarket() public {
+        vm.expectRevert("Sell is not allowed if you don't own shares");
+        market.createOrder(BuySell.SELL, YesNo.YES, LimitMarket.MARKET, 100, 100);
+    }
+
+    // SELL-NO-LIMIT (should revert)
+    function test_Market_createOrder_SellNoLimit() public {
+        vm.expectRevert("Sell is not allowed if you don't own shares");
+        market.createOrder(BuySell.SELL, YesNo.NO, LimitMarket.LIMIT, 100, 100);
+    }
+
+    // SELL-NO-MARKET (should revert)
+    function test_Market_createOrder_SellNoMarket() public {
+        vm.expectRevert("Sell is not allowed if you don't own shares");
+        market.createOrder(BuySell.SELL, YesNo.NO, LimitMarket.MARKET, 100, 100);
     }
 }
