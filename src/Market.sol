aecfa9a3cf35fd54090bba0d7c5ef7098f7607ab
lazaralex98 <alex2012ro@gmail.com>
2025-05-20 16:56:31 +0300

    do not match orders from the same user

--- src/Market.sol
+++ src/Market.sol
@@ -101,6 +101,10 @@ contract Market {
             revert("Only the owner can match orders");
         }
 
+        if (_user1 == _user2) {
+            revert("Cannot match orders for the same user");
+        }
+
         Order storage order1 = orders[_user1][_orderId1];
         Order storage order2 = orders[_user2][_orderId2];
         if (order1.user == address(0) || order2.user == address(0)) {
